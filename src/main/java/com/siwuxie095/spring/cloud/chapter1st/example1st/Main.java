7be4a3bfb54dba1bdb34200cc0214d22c12cae78
siwuxie095 <834879583@qq.com>
2021-05-06 22:26:54 +0800

    欢迎来到 Cloud 和 Spring；

---
+++ src/main/java/com/siwuxie095/spring/cloud/chapter1st/example1st/Main.java
@@ -0,0 +1,24 @@
+package com.siwuxie095.spring.cloud.chapter1st.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-05-06 08:16:48
+ */
+public class Main {
+
+    /**
+     * 欢迎来到 Cloud 和 Spring
+     *
+     * 软件开发领域中永恒不变的就是作为软件开发人员坐在无限混乱和变化的世界中。随着新技术和新方法的突然出现，
+     * 这使开发者重新评估如何为客户建立和交付解决方案。这样的例子，比如使用微服务构建应用的方式被许多企业快
+     * 速采用。微服务是分布的、松散耦合的、单一职责的软件服务。
+     *
+     * 这里会向你介绍微服务架构和为什么你应该考虑使用它们构建你的应用程序。将会看到如何使用 java，Spring
+     * Boot 和 Spring Cloud 构建微服务。如果你是一个 java 开发者，Spring Boot 和 Spring Cloud 将为
+     * 你提供一个将传统的、庞大的 Spring 应用迁移到可以部署到云的微服务应用的简单迁移路径。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
