3dc4e9c67fc5f163f90867eb0fb0bb2c0222e9ae
siwuxie095 <834879583@qq.com>
2021-05-07 08:41:05 +0800

    什么是微服务；

--- src/main/java/com/siwuxie095/spring/cloud/chapter1st/example1st/Main.java
+++ src/main/java/com/siwuxie095/spring/cloud/chapter1st/example1st/Main.java
@@ -13,8 +13,8 @@ public class Main {
      * 这使开发者重新评估如何为客户建立和交付解决方案。这样的例子，比如使用微服务构建应用的方式被许多企业快
      * 速采用。微服务是分布的、松散耦合的、单一职责的软件服务。
      *
-     * 这里会向你介绍微服务架构和为什么你应该考虑使用它们构建你的应用程序。将会看到如何使用 java，Spring
-     * Boot 和 Spring Cloud 构建微服务。如果你是一个 java 开发者，Spring Boot 和 Spring Cloud 将为
+     * 这里会向你介绍微服务架构和为什么你应该考虑使用它们构建你的应用程序。将会看到如何使用 Java，Spring
+     * Boot 和 Spring Cloud 构建微服务。如果你是一个 Java 开发者，Spring Boot 和 Spring Cloud 将为
      * 你提供一个将传统的、庞大的 Spring 应用迁移到可以部署到云的微服务应用的简单迁移路径。
      */
     public static void main(String[] args) {
