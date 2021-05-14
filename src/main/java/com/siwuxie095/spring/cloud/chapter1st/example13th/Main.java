a24e10c53926194b77ac579f7e5f07d7f02e5aba
siwuxie095 <834879583@qq.com>
2021-05-14 22:14:42 +0800

    小结；

---
+++ src/main/java/com/siwuxie095/spring/cloud/chapter1st/example13th/Main.java
@@ -0,0 +1,28 @@
+package com.siwuxie095.spring.cloud.chapter1st.example13th;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-05-14 22:08:57
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * （1）微服务是负责一个特定区域的功能，范围非常小的块。
+     * （2）对于微服务没有行业标准。不同于其他早期的 Web 服务协议，微服务采取原则为基础的方法
+     * 和结合 REST 和 JSON 的概念。
+     * （3）写微服务是容易的，但在生产完全投入运行它们，还需要更多的考虑。这里介绍了微服务开发
+     * 模式的几个类别，包括核心开发、路由、客户端弹性、安全、日志、和构建/部署模式。
+     * （4）微服务是语言无关的，这里介绍了两种有助于建设微服务的 Spring 框架：Spring Boot
+     * 和 Spring Cloud。
+     * （5）Spring Boot 被用来简化构建基于 REST/JSON 的微服务。它的目标是使你只不过使用很少
+     * 的注解就能快速建立微服务。
+     * （6）Spring Cloud 是一个来自开源技术公司的工具集，如 Netflix 和 HashiCorp，他们已
+     * 经用 Spring 注解包装，大大简化这些服务的安装和配置。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
