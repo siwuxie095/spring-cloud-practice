3b2000ab1fadf95761f071026c962614b1e30703
siwuxie095 <834879583@qq.com>
2021-05-29 22:56:39 +0800

    小结；

---
+++ src/main/java/com/siwuxie095/spring/cloud/chapter3th/example7th/Main.java
@@ -0,0 +1,23 @@
+package com.siwuxie095.spring.cloud.chapter3th.example7th;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-05-29 22:54:25
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * （1）Spring Cloud 配置服务器允许你设置具有特定于环境的应用程序属性值。
+     * （2）Spring 使用 Spring 概要文件启动一个服务，以确定从 Spring Cloud 配置服务中
+     * 检索到哪些环境属性。
+     * （3）Spring Cloud 配置服务可以使用基于文件或基于 Git 的应用程序配置存储库来存储
+     * 应用程序属性。
+     * （4）Spring Cloud 配置服务允许你使用对称和非对称加密方式加密敏感属性文件。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
