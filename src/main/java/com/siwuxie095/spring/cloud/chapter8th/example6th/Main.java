8caa81eb5e6f07b77722a10a79871233f9c514db
siwuxie095 <834879583@qq.com>
2021-06-22 22:32:34 +0800

    小结；

---
+++ src/main/java/com/siwuxie095/spring/cloud/chapter8th/example6th/Main.java
@@ -0,0 +1,26 @@
+package com.siwuxie095.spring.cloud.chapter8th.example6th;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-06-22 22:30:15
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * （1）使用消息传递的异步通信是微服务架构的关键部分。
+     * （2）在应用程序中使用消息传递可以使服务能够伸缩并且变得更具容错性。
+     * （3）Spring Cloud Stream 通过使用简单的注解以及抽象出底层消息平台的特定平台细节
+     * 来简化消息的生产和消费。
+     * （4）Spring Cloud Stream 消息发射器是一个带注解的 Java 方法，用于将消息发布到
+     * 消息代理的队列中。
+     * （5）Spring Cloud Stream 消息接收器是一个带注解的 Java 方法，它接收消息代理队
+     * 列上的消息。
+     * （6）Redis 是一个键值存储，它可以用作数据库和缓存。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
