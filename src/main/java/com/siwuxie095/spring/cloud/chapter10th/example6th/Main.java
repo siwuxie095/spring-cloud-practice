fa4662e01b2db9a487a4abd860f0acb12fc908b4
siwuxie095 <834879583@qq.com>
2021-06-26 22:54:54 +0800

    开始构建和部署管道：GitHub 和 Travis CI；

---
+++ src/main/java/com/siwuxie095/spring/cloud/chapter10th/example6th/Main.java
@@ -0,0 +1,28 @@
+package com.siwuxie095.spring.cloud.chapter10th.example6th;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-06-26 22:50:22
+ */
+public class Main {
+
+    /**
+     * 开始构建和部署管道：GitHub 和 Travis CI
+     *
+     * 有数十种源代码管理引擎和构建部署引擎（包括内部部署和基于云的）可以实现构建和部署管道。对于这里的示例，
+     * 特意选择了 GitHub 作为源代码控制库，并使用 Travis CI 作为构建引擎。Git 源代码控制库是非常流行的
+     * 代码库，GitHub 是当今最大的基于云的源代码控制库之一。
+     *
+     * Travis CI 是一个与 GitHub 紧密集成的构建引擎（它也支持 Subversion 和 Mercurial）。它非常容易
+     * 使用，并完全由项目的根目录中的单个配置文件（.travis.yml）驱动。Travis CI 的简单性使得建立一个简
+     * 单的构建管道变得非常容易。
+     *
+     * 到目前为止，这里的所有代码示例都可以从桌面单独运行（除了连接到 GitHub 之外）。在这里，如果你想完全
+     * 遵循代码示例，则需要创建自己的 GitHub、Travis CI 和 Docker Hub 账户。这里不会介绍如何创建这些
+     * 账户，但个人 Travis CI 账户和 GitHub 账户的建立都可以从 Travis CI 网页完成。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
