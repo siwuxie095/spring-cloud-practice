f54f2e02b16332918ebb9888ea6bfee14fc31a79
siwuxie095 <834879583@qq.com>
2021-06-27 12:35:40 +0800

    从 GitHub 下载项目；

---
+++ src/main/java/com/siwuxie095/spring/cloud/extend1st/example3rd/Main.java
@@ -0,0 +1,32 @@
+package com.siwuxie095.spring.cloud.extend1st.example3rd;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-06-27 12:26:34
+ */
+public class Main {
+
+    /**
+     * 从 GitHub 下载项目
+     *
+     * 这里的所有源代码都在 GitHub 存储库（http://github.com/carnellj）中。每一部分都有自己的源代码
+     * 存储库。下面是这里使用的所有 GitHub 存储库的清单。
+     * 第 1 部分：http://github.com/carnellj/spmia-chapter1。
+     * 第 2 部分：http://github.com/carnellj/spmia-chapter2。
+     * 第 3 部分：http://github.com/carnellj/spmia-chapter3 和 http://github.com/carnellj/config-repo。
+     * 第 4 部分：http://github.com/carnellj/spmia-chapter4。
+     * 第 5 部分：http://github.com/carnellj/spmia-chapter5。
+     * 第 6 部分：http://github.com/carnellj/spmia-chapter6。
+     * 第 7 部分：http://github.com/carnellj/spmia-chapter7。
+     * 第 8 部分：http://github.com/carnellj/spmia-chapter8。
+     * 第 9 部分：http://github.com/carnellj/spmia-chapter9。
+     * 第 10 部分：http://github.com/carnellj/spmia-chapter10 和 http://github.com/carnellj/chapter-10-platform-tests。
+     *
+     * 通过 GitHub，你可以使用 Web UI 将文件作为 zip 文件进行下载，或者通过 git clone 命令进行下载。
+     *
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
