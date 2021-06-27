de7329668f0f33fff24b3f58087ec1e9620001ca
siwuxie095 <834879583@qq.com>
2021-06-27 12:23:53 +0800

    所需的软件；

---
+++ src/main/java/com/siwuxie095/spring/cloud/extend1st/example2nd/Main.java
@@ -0,0 +1,28 @@
+package com.siwuxie095.spring.cloud.extend1st.example2nd;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-06-27 12:20:07
+ */
+public class Main {
+
+    /**
+     * 所需的软件
+     *
+     * 要为所有部分构建软件，你需要在桌面上安装下列软件。注意，这些是这里所用的软件的版本。这些软件的其他版本可能
+     * 也能运行，但以下软件是这里用来构建本书代码的。
+     * （1）Apache Maven：这里使用的是 Maven 的 3.3.9 版本。之所以选择 Maven，是因为虽然其他构建工具（如
+     * Gradle）非常流行，但 Maven 仍然是 Java 生态系统中使用的主要构建工具。这里的所有代码示例都是使用 Java
+     * 1.8 版进行编译的。
+     * （2）Docker：这里使用 Docker 的 1.12 版本构建代码示例。这里的代码示例可以用早期版本的 Docker 处理，
+     * 但是如果你想在 Docker 的早期版本中使用这些代码，可能必须切换到版本 1 的 docker-compose 链接格式。
+     * （3）Git 客户端：这里的所有源代码都存储在一个 GitHub 存储库中。在这里使用了 Git 客户端的 2.8.4 版本。
+     *
+     * 这里不打算一一介绍如何安装这些组件。上面列出的每个软件包都有简单的安装指导，它们应该很容易安装。Docker
+     * 有一个用于安装的 GUI 客户端。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
