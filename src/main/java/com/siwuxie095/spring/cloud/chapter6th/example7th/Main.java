4c6de4d7b839a4cf23c5ca353295363d1ad24065
siwuxie095 <834879583@qq.com>
2021-07-10 15:01:14 +0800

    构建接收关联 ID 的后置过滤器；

--- src/main/java/com/siwuxie095/spring/cloud/chapter6th/example7th/Main.java
+++ src/main/java/com/siwuxie095/spring/cloud/chapter6th/example7th/Main.java
@@ -8,24 +8,26 @@ package com.siwuxie095.spring.cloud.chapter6th.example7th;
 public class Main {
 
     /**
-     * 创建 post 类型 Zuul 过滤器接收关联 ID
+     * 构建接收关联 ID 的后置过滤器
      *
-     * 记住，Zuul 代表服务客户端执行实际的 HTTP 调用。Zuul 有机会检查从目标服务调用返回的响应并修改或使用
-     * 附加的信息装饰响应。当加上使用前置过滤器采集数据时，一个 Zuul 后置过滤器是收集数据和完成与用户的事务
-     * 相关的任何记录的理想地点。你要利用这个来注入关联 ID，你已经通过你的微服务返回到用户。
+     * 记住，Zuul 代表服务客户端执行实际的 HTTP 调用。Zuul 有机会从目标服务调用中检查响应，然后修改响应或以额外
+     * 的信息装饰它。当与以前置过滤器捕获数据相结合时，Zuul 后置过滤器是收集指标并完成与用户事务相关联的日志记录
+     * 的理想场所。这里将利用这一点，通过将已经传递给微服务的关联 ID 注入回用户。
      *
-     * 你打算通过使用一个 Zuul 后置过滤器注入关联 ID 到正在被传递回给服务调用者的 HTTP 响应头。通过这种方
-     * 式，你可以将关联 ID 传递给调用者，而无需接触消息体。如下所示。
+     * 这里将使用 Zuul 后置过滤器将关联 ID 注入 HTTP 响应首部中，该 HTTP 响应首部传回给服务调用者。这样，就可
+     * 以将关联 ID 传回给调用者，而无需接触消息体。如下是构建后置过滤器的代码。
      *
      * @Component
      * public class ResponseFilter extends ZuulFilter {
-     *     private static final int  FILTER_ORDER=1;
-     *     private static final boolean  SHOULD_FILTER=true;
+     *
+     *     private static final int  FILTER_ORDER = 1;
+     *     private static final boolean  SHOULD_FILTER = true;
      *     private static final Logger logger = LoggerFactory.getLogger(ResponseFilter.class);
      *
      *     @Autowired
      *     FilterUtils filterUtils;
      *
+     *     // 要构建一个后置过滤器，需要设置过滤器的类型为 POST_FILTER_TYPE
      *     @Override
      *     public String filterType() {
      *         return FilterUtils.POST_FILTER_TYPE;
@@ -46,22 +48,26 @@ public class Main {
      *         RequestContext ctx = RequestContext.getCurrentContext();
      *
      *         logger.debug("Adding the correlation id to the outbound headers. {}",
-     *         filterUtils.getCorrelationId());
-     *         ctx.getResponse().addHeader(FilterUtils.CORRELATION_ID,
-     *         filterUtils.getCorrelationId());
+     *                 filterUtils.getCorrelationId());
      *
+     *         // 获取原始 HTTP 请求中传入的关联 ID，并将它注入响应中
+     *         ctx.getResponse().addHeader(FilterUtils.CORRELATION_ID,
+     *                 filterUtils.getCorrelationId());
+     *
+     *         // 记录传出的请求 URI，这样就有了 "书挡"，它将显示进入 Zuul 的用户请求的传入和传出条目
      *         logger.debug("Completing outgoing request for {}.",
-     *         ctx.getRequest().getRequestURI());
+     *                 ctx.getRequest().getRequestURI());
      *
      *         return null;
      *     }
+     *
      * }
      *
-     * ResponseFilter 实现后，你可以启动你的 Zuul 服务，并通过它调用 EagleEye 的许可服务。一旦服务完成
-     * 后，你会在调用的 HTTP 响应头看到一个 tmx-correlation-id。
+     * 实现完 ResponseFilter 之后，就可以启动 Zuul 服务，并通过它调用 EagleEye 许可证服务。服务完成后，就可以
+     * 在调用的 HTTP 响应首部上看到一个 tmx-correlation-id。
      *
-     * 到目前为此，所有的过滤器示例都处理了在路由到目标目的地之前和之后的服务客户端调用。后续将看看最后一个
-     * 过滤器示例，如何动态地更改你要将用户发送到的目标路由。
+     * 到目前为止，所有的过滤器示例都是在路由到目的地之前或之后对服务客户端调用进行操作。后续将看看最后一个过滤器示
+     * 例，如何动态地更改用户要到达的目标路径。
      */
     public static void main(String[] args) {
 
